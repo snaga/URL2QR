fe2a21cd3d50c8311865db502b46eec81adfba37
Satoshi Nagayasu <snaga@uptime.jp>
2016-06-16 15:50:44 +0900

    URL2QR first version.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #URL2QR
-A basic add-on
+
+Show a QR code of the URL in the active tab.
+
+
+## Icon Credit
+
+Icons made by [Freepik](http://www.freepik.com) from [Flaticon www.flaticon.com](http://www.flaticon.com) is licensed by [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/) 
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-var self = require("sdk/self");
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+var cm = require("sdk/context-menu");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var button = buttons.ActionButton({
+  id: "url2qr",
+  label: "Convert URL to QR",
+  icon: {
+    "16": "./qr-code-16.png",
+    "32": "./qr-code-32.png",
+    "64": "./qr-code-64.png"
+  },
+  onClick: handleClick
+});
+
+var qr_panel = require("sdk/panel").Panel({
+  contentURL: 'https://chart.googleapis.com/chart?cht=qr&chs=320x320&chl=https://developer.mozilla.org/',
+  height: 360,
+  width: 360
+});
+
+qr_panel.on("show", function() {
+  qr_panel.port.emit("show");
+});
+
+require("sdk/context-menu").Item({
+  label: "Convert URL to QR",
+  contentScript: 'self.on("click", function (node, data) {' +
+                 " self.postMessage(node.src);" +
+                 '});',
+  onMessage: function(src) {
+    show_qr_panel();
+  }
+  });
+
+function handleClick(state) {
+  show_qr_panel();
 }
 
-exports.dummy = dummy;
+function show_qr_panel() {
+    console.log("Active Tab: " + tabs.activeTab.url)
+    qr_panel.contentURL = "https://chart.googleapis.com/chart?cht=qr&chs=320x320&chl=" + tabs.activeTab.url;
+    qr_panel.show();
+}
